853a96c6f08abb5ddd6aca9d9cc1095b079d9937
sidmohanty11 <sidmohanty11@gmail.com>
2021-05-03 21:17:03 +0530

    cart button animation using useeffect

--- src/components/Layout/HeaderButton.js
+++ src/components/Layout/HeaderButton.js
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react'
+import React, { useContext, useEffect, useState } from 'react';
 import CartIcon from '../../assets/shopping-cart.png';
 import classes from './HeaderButton.module.css';
 import CartContext from '../../store/cart-context';
@@ -6,14 +6,28 @@ import CartContext from '../../store/cart-context';
 //The header button
 
 const HeaderButton = (props) => {
+    const [btnBump, setBtnBump] = useState(false);
     const cartCtx = useContext(CartContext);
-
-    const numberOfItems = cartCtx.items.reduce((curNum, item) => {
+    const { items } = cartCtx;
+    const numberOfItems = items.reduce((curNum, item) => {
         return +(curNum) + +(item.amount);
     }, 0);
 
+    const btnClasses = `${classes.button} ${btnBump ? classes.bump : ''}`;
+
+    useEffect(() => {
+        if (cartCtx.items.length === 0) { return; }
+        setBtnBump(true);
+
+        const timer = setTimeout(() => { setBtnBump(false); }, 300);
+
+        return () => {
+            clearTimeout(timer);
+        }
+    },[items]);
+
     return (
-        <button className={classes.button} onClick={props.onClick}>
+        <button className={btnClasses} onClick={props.onClick}>
             <span className={classes.icon}>
                 <img src={CartIcon} />
             </span>
