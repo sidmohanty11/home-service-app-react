f2b2c5b7e27cb5f7fcc542429e7b0a7e5d99d34f
sidmohanty11 <sidmohanty11@gmail.com>
2021-05-05 20:30:56 +0530

    error handling and loading

--- src/components/HomeServices/AvailableHS.js
+++ src/components/HomeServices/AvailableHS.js
@@ -1,56 +1,61 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Card from '../UI/Card';
 import classes from './AvailableHS.module.css';
 import HSItem from './HSItem';
 
-const DUMMY_HS = [
-    {
-        id: 'h1',
-        name: 'Plumbers Organization',
-        description: 'Any plumber related work',
-        price: 300,
-    },
-    {
-        id: 'h2',
-        name: 'Computer/Mobile Repair Organization',
-        description: 'Any software/hardware related work',
-        price: 300,
-    },
-    {
-        id: 'h3',
-        name: 'Electricians Organization',
-        description: 'For any electricity stuffs',
-        price: 300,
-    },
-    {
-        id: 'h4',
-        name: 'You need Cook Organization',
-        description: 'If you need cook',
-        price: 300,
-    },
-    {
-        id: 'h5',
-        name: 'Maid Organization',
-        description: 'If you need maid',
-        price: 300,
-    },
-];
-
 //The no. of available home services available wrapped around a Card.
 
 const AvailableHS = () => {
+    const [hs, setHs] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [httpErr, setHttpErr] = useState(false);
+
+    //----------fetching data from firebase server!-------------
+    useEffect(() => {
+        const fetchData = async () => {
+            const res = await fetch('https://react-http-8d73b-default-rtdb.firebaseio.com/jugaad.json');
+
+            if (!res.ok) {
+                throw new Error('Something went wrong from server!');
+            }
+            const resData = await res.json();
+            const loadedData = [];
+            for (const key in resData) {
+                loadedData.push({
+                    id: key,
+                    name: resData[key].name,
+                    description: resData[key].description,
+                    price: resData[key].price
+                });
+            }
+            setHs(loadedData);
+            setIsLoading(false);
+        };
+        //---can't use try catch as we have to make useEffect async fn---
+        //-------------can make this a fn itself-------------------------
+        //--------------or use then catch--------------------------------
+        fetchData()
+            .then()
+            .catch(e => {
+            setIsLoading(false);
+            setHttpErr(e.message);
+            });
+    }, []);
     return (
         <div className={classes.hs}>
             <Card>
                 <ul>
-                    {DUMMY_HS.map(hs => <HSItem
-                        id={hs.id}
-                        key={hs.id}
-                        name={hs.name}
-                        description={hs.description}
-                        price={hs.price} />
+                    {hs.map(h =>
+                        <HSItem
+                        id={h.id}
+                        key={h.id}
+                        name={h.name}
+                        description={h.description}
+                        price={h.price} />
                     )}
                 </ul>
+                {isLoading && !httpErr && <p className={classes.loading}>Loading...</p>}
+                {httpErr && <p className={classes.httpError}>{httpErr}</p>}
             </Card>
         </div>
     )
